b1d1d988e9cad5b04102ea08e99ae60a1eb17656
Andrew Hickman <andrew.hickman1@sky.com>
2026-05-22 22:02:48 +0100

    wip

--- wdn-render/src/tile/wall.rs
+++ wdn-render/src/tile/wall.rs
@@ -2,7 +2,7 @@ use wdn_physics::tile::storage::{DoorAdjacency, TileMaterial, WallAdjacency};
 
 use crate::tile::WALL_OFFSET;
 
-pub fn wall_sprite_offset(solid: bool, adjacency: WallAdjacency) -> u16 {
+// pub fn wall_sprite_offset(solid: bool, walls: WallAdjacency, doors: DoorAdjacency) -> u16 {
     //     const EMPTY_LOOKUP: [u8; 256] = [
     //         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
     //         2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4,
@@ -38,6 +38,18 @@ pub fn wall_sprite_offset(solid: bool, adjacency: WallAdjacency) -> u16 {
     //     WALL_OFFSET + offset
 
     todo!()
+// }
+
+fn empty_sprite_offset(walls: WallAdjacency) -> u16 {
+    todo!()
+}
+
+fn wall_sprite_offset(walls: WallAdjacency, doors: DoorAdjacency) -> u16 {
+    todo!()
+}
+
+fn door_sprite_offset(walls: WallAdjacency, doors: DoorAdjacency) -> u16 {
+    todo!()
 }
 
 #[test]
@@ -52,12 +64,14 @@ fn test_tile_sprite_index() {
             for j in 0..=255u8 {
                 let walls = WallAdjacency::from_bits_retain(j);
 
+                let mut normal_material = material;
                 let mut normal_walls = walls.intersection(
                     WallAdjacency::SOUTH
                         | WallAdjacency::SOUTH_WEST
                         | WallAdjacency::SOUTH_EAST
                         | WallAdjacency::WEST
-                        | WallAdjacency::EAST,
+                        | WallAdjacency::EAST
+                        | WallAdjacency::NORTH,
                 );
                 let mut normal_doors = doors
                     .intersection(DoorAdjacency::SOUTH | DoorAdjacency::WEST | DoorAdjacency::EAST);
@@ -76,6 +90,8 @@ fn test_tile_sprite_index() {
 
                 match material {
                     TileMaterial::Empty => {
+                        normal_walls.remove(WallAdjacency::NORTH);
+
                         if !normal_walls.contains(WallAdjacency::SOUTH) {
                             normal_walls = WallAdjacency::NONE;
                         }
@@ -84,6 +100,8 @@ fn test_tile_sprite_index() {
                         normal_doors = DoorAdjacency::NONE;
                     }
                     TileMaterial::Wall => {
+                        normal_walls.remove(WallAdjacency::NORTH);
+
                         if !normal_walls.contains(WallAdjacency::SOUTH) {
                             normal_walls
                                 .remove(WallAdjacency::SOUTH_WEST | WallAdjacency::SOUTH_EAST);
@@ -91,13 +109,23 @@ fn test_tile_sprite_index() {
                     }
                     TileMaterial::Door => {
                         normal_walls.remove(WallAdjacency::WEST | WallAdjacency::EAST);
+
+                        if !normal_walls.contains(WallAdjacency::SOUTH) {
+                            normal_walls
+                                .remove(WallAdjacency::SOUTH_WEST | WallAdjacency::SOUTH_EAST);
+                        }
+
+                        if normal_walls.is_empty() {
+                            normal_material = TileMaterial::Empty;
+                        }
+
                         normal_doors = DoorAdjacency::NONE;
                     }
                 }
 
                 let index = patterns.len() as u16;
 
-                match patterns.entry((material, normal_walls, normal_doors)) {
+                match patterns.entry((normal_material, normal_walls, normal_doors)) {
                     hash_map::Entry::Occupied(entry) => {
                         // assert_eq!(
                         //     wall_sprite_offset(solid, walls),
@@ -107,7 +135,7 @@ fn test_tile_sprite_index() {
                     }
                     hash_map::Entry::Vacant(entry) => {
                         println!(
-                            "{}: material={material:?} walls={normal_walls:?}, doors={normal_doors:?}",
+                            "{}: material={normal_material:?} walls={normal_walls:?}, doors={normal_doors:?}",
                             index + 1
                         );
                         // assert_eq!(
