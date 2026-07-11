a53c7793c2016d53772d7cc80f72f22441ac0574
Andrew Hickman <andrew.hickman1@sky.com>
2026-07-11 23:19:09 +0100

    wip stair sprites

--- wdn-colorgen/src/main.rs
+++ wdn-colorgen/src/main.rs
@@ -2,7 +2,7 @@ use bevy_color::{Mix, Oklaba, Srgba};
 
 pub const SHADOW: Oklaba = Oklaba::new(0.18, -0.025, -0.035, 1.0);
 
-pub const WALL: Srgba = Srgba::rgb(0.38431373, 0.36078432, 0.3254902);
+pub const WALL: Srgba = Srgba::rgb(0.44313726, 0.41960785, 0.38039216);
 pub const FLOOR: Srgba = Srgba::rgb(0.654902, 0.6666667, 0.6392157);
 pub const DIRT: Srgba = Srgba::rgb(0.38431373, 0.36078432, 0.3254902);
 pub const GRASS: Srgba = Srgba::rgb(0.37254903, 0.4627451, 0.3137255);
@@ -17,5 +17,17 @@ pub fn shade(base: Srgba, amount: f32) -> Srgba {
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "WALL: top: {}, front: {}, side: {}, occluded: {}",
+        WALL.to_hex(),
+        shade(WALL, 0.1).to_hex(),
+        shade(WALL, 0.35).to_hex(),
+        shade(WALL, 0.65).to_hex()
+    );
+    println!(
+        "FLOOR: top: {}, front: {}, side: {}",
+        FLOOR.to_hex(),
+        shade(FLOOR, 0.35).to_hex(),
+        shade(FLOOR, 0.65).to_hex(),
+    );
 }
